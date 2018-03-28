c1b7bdfd48fde817218de232541000ef316cb32b
skconan <supakit.kr@gmail.com>
2018-03-28 22:51:12 +0700

    kangaroo(E)

--- kangaroo.py
+++ kangaroo.py
@@ -4,10 +4,10 @@ import sys
 
 
 def kangaroo(x1, v1, x2, v2):
-    if v1 == 0 or v2 == 0:
+    if v1 == 0 or v2 == 0 or v1 == v2:
         return 'NO'
     res = (x2 * v1 - x1 * v2) / ((v1 - v2) * 1.0)
-    if res == int(res) and res >= 0 and res >= x1 and res >= x2 and (int(res) - x1)%v1 + (int(res) - x2)%v2 == 0:
+    if res == int(res) and res >= 0 and res >= x1 and res >= x2 and (int(res) - x1) % v1 + (int(res) - x2) % v2 == 0:
         return 'YES'
     else:
         return 'NO'
